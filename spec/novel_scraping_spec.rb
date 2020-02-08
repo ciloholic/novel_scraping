5b8fdcd487ba4ff51530e57eba85acb780974029
ciloholic <ciloholic0119@gmail.com>
2020-02-09 01:14:07 +0900

    add gem template

---
+++ spec/novel_scraping_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe NovelScraping do
+  it 'has a version number' do
+    expect(NovelScraping::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
